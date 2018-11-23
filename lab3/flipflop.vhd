9326ff5ca04620d7b441b1264ae5bade1cd8bec4
stf98 <43681433+stf98@users.noreply.github.com>
2018-11-22 20:16:22 -0500

    Update flipflop.vhd

--- lab3/flipflop.vhd
+++ lab3/flipflop.vhd
@@ -8,9 +8,9 @@ end flipflop;
 architecture flipflop_arch of flipflop is
 begin
 	process (clk) begin
-		if rising_edge(clk) then
-			Q<=D;
-		end if;
+		if rising_edge(clk) then  --multivibrador bi estable que sirve para almacenar, en este caso 1 bit
+			Q<=D;		--este bit representa los estados entre los que se cambia
+		end if;			--si hay una pendiente de subida, a la salida Q se le asigna entrada D
 	end process;
 end flipflop_arch;
 	
